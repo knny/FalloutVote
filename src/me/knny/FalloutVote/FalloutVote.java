f334f36a70813a9a05ed0752392c398c7549caad
Daniel Mudd <spudstabber@gmail.com>
2012-08-31 23:59:26 +0100

    Formatting changes and other stuff.

--- src/me/knny/FalloutVote/FalloutVote.java
+++ src/me/knny/FalloutVote/FalloutVote.java
@@ -10,38 +10,41 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class FalloutVote extends JavaPlugin {
 	
-	final Logger log = Logger.getLogger("Minecraft");
+	private static final Logger log = Logger.getLogger("Minecraft");
 	
-	public void onEnable () {
-		
-		log.info("[FalloutVote] FalloutVote enabled!");
-		
+	public void onEnable () 
+	{
+		log.info("[FalloutVote] FalloutVote enabled!");	
 	}
 	
-	public void onDisable () {
-		
-		log.info("[FalloutVote] FalloutVote disabled!");
-		
+	public void onDisable () 
+	{
+		log.info("[FalloutVote] FalloutVote disabled!");	
 	}
 	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String CommandLabel, String[] args) {
-		final Player player = (Player) sender;
-		if(cmd.getName().equalsIgnoreCase("vote")) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		if (!(sender instanceof Player)) return false;
+
+		Player player = (Player) sender;
+
+		if(cmd.getName().equalsIgnoreCase("vote")) 
+		{
 			if(!player.hasPermission("vote.command"));
+			{
 				player.sendMessage(ChatColor.RED + "You don't have permission to do this.");
 				return true;
-				
-		    }
-		    player.sendMessage(ChatColor.YELLOW + player.getName() + ", please vote for us at the following sites. You will get one emerald per vote." +
-		    		"" +
-		    		"Planet Minecraft  - www.test.com" +
-		    		"Minestatus        - www.test.com" +
-		    		"Best Game Servers - www.test.com" +
-		    		"" +
+			}
+			else
+			{
+		    	player.sendMessage(ChatColor.YELLOW + player.getName() + ", please vote for us at the following sites. You will get one emerald per vote.\n" +
+		    		"Planet Minecraft  - www.test.com\n" +
+		    		"Minestatus        - www.test.com\n" +
+		    		"Best Game Servers - www.test.com\n" +
 		    		"You may vote every 24 hours - Thanks for voting!");
-		    return false;
-		    
+		    	return true;
+		    }
+		}
 	}
 	
 }
